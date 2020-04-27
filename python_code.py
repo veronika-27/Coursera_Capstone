17b7346b62f1a36f5e383929b4b0edfcdffcfd39
veronika <vkz_27@abv.bg>
2020-04-27 21:29:29 +0300

    adding a python script that imports pandas and numpy and ptints a message greeting the course

---
+++ python_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project.
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print("Hello Capstone Project Course!")
+
